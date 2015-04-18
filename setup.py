69b0a89d4a841bc52a7b47c8ee1c65d4fe836e75
Vladimir Bolshakov <vovanbo@gmail.com>
2015-04-18 11:55:29 +0300

    Python 3 support in setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
-#coding: utf-8
-from distutils.core import setup
+# coding: utf-8
+from __future__ import absolute_import, unicode_literals
+
+import sys
+
+PY3 = sys.version_info >= (3, 0)
 
 for cmd in ('egg_info', 'develop'):
-    import sys
+
     if cmd in sys.argv:
         from setuptools import setup
 
-import sys
-reload(sys).setdefaultencoding("UTF-8")
+if not PY3:
+    reload(sys).setdefaultencoding("UTF-8")
+
+
+description = 'Приложение для интеграции платежной системы ROBOKASSA в проекты на Django.'
+long_description = open('README.rst').read() + "\n\n" + open('CHANGES.rst').read()
+
 
 setup(
     name='django-robokassa',
@@ -19,11 +28,11 @@ setup(
     packages=['robokassa', 'robokassa.migrations'],
 
     url='https://bitbucket.org/kmike/django-robokassa/',
-    license = 'MIT license',
-    description = u'Приложение для интеграции платежной системы ROBOKASSA в проекты на Django.'.encode('utf8'),
-    long_description = open('README.rst').read().decode('utf8') + u"\n\n" + open('CHANGES.rst').read().decode('utf8'),
+    license='MIT license',
+    description=description if PY3 else description.encode('utf8'),
+    long_description=long_description if PY3 else long_description.decode('utf8'),
 
-    requires=['django (>= 1.3)'],
+    install_requires=['Django>=1.7'],
 
     classifiers=(
         'Development Status :: 4 - Beta',
@@ -35,6 +44,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Natural Language :: Russian',
     ),
